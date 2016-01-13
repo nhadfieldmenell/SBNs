fee25d896243fb7567422d7ff3bc3a81e1323165
Noah <noah@ark.com>
2016-01-13 12:24:55 -0800

    printing paths

--- sdd-graph/test_grids_minato.py
+++ sdd-graph/test_grids_minato.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
 
     start,goal = 1,(dimension[0]+1)*(dimension[1]+1)
     paths = GraphSet.paths(start, goal)
-    tl.draw(paths.choice())
+    #tl.draw(paths.choice())
     print GraphSet
     print paths.len()
 

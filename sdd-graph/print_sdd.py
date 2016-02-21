ceac31eae5fdc3e88987a6b7919b5247572b3d8f
Noah <noah@ark.com>
2016-02-20 21:37:12 -0800

    print_sdd

--- sdd-graph/print_sdd.py
+++ sdd-graph/print_sdd.py
@@ -1,2 +1,29 @@
-import test_grids_minato as grid
+#!/usr/bin/env python
+
+import test_graphs as g
+import sys
+import pickle
+import sdd
+
+
+
+if __name__ == '__main__':
+	
+    import sys
+
+    if len(sys.argv) != 4:
+        print "usage: %s [GRID-M] [GRID-N]" % sys.argv[0]
+        exit(1)
+
+    m,n = (int(sys.argv[1]),int(sys.argv[2]))
+    fnPrefix = ("asdf-%d-%d.zdd" % (m,n))
+    gFn = ("asdf-%d-%d.graph.pickle" % (m,n))
+
+    vtree = sdd.sdd_vtree_read('%s.vtree' % fnPrefix)
+    manager = sdd.sdd_manager_new(vtree)
+    sdd.sdd_vtree_free(vtree)
+    alpha = sdd.sdd_read(('%s.sdd' % fnPrefix),manager)
+    graph = pickle.load(open(gFn),'rb')
+
+    g.print_grids(alpha,m,n,graph,manager)
 

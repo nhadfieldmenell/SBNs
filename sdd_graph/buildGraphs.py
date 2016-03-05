e81c8d7b6c05a5e9e15f50612b05346742fffb55
Noah <noah@ark.com>
2016-03-05 15:06:47 -0800

    sdd size/model counts

--- sdd_graph/buildGraphs.py
+++ sdd_graph/buildGraphs.py
@@ -307,8 +307,8 @@ def main():
     min_lon = -122.46
     max_lon = -122.39
 
-    rows = 10 
-    cols = 10 
+    rows = 11 
+    cols = 11 
     g = Graph(min_lat,max_lat,min_lon,max_lon,rows,cols)
     try_lat = 37.721396 
     try_lon = -122.400256

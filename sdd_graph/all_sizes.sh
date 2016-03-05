4cac524b77fed8c736c5871187f773d0db6dd717
Noah <noah@ark.com>
2016-03-05 14:51:06 -0800

    sdd size/model counts

--- sdd_graph/all_sizes.sh
+++ sdd_graph/all_sizes.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for i in 2 3 4 5 6 7 8 9 10 11 12
+for i in 4 10 11 12 # 3 4 5 6 7 8 9 10 11 12
 do 
-    echo $(((i*i)/2))
-    python test_grids_minato.py $i $i $(((i*i)/2))
+    echo $((((i*i)/2)+(i/2)))
+    time python test_grids_minato.py $i $i $((((i*i)/2)+(i/2)))
     python minato2sdd.py $i $i
     python print_sdd.py $i $i
 done

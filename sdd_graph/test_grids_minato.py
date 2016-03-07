ae55eb146f1f41dfc9ba2ee82e093f5ca987fb6c
Noah <noah@ark.com>
2016-03-07 10:48:24 -0800

    testing/training basic working

--- sdd_graph/test_grids_minato.py
+++ sdd_graph/test_grids_minato.py
@@ -83,8 +83,8 @@ if __name__ == '__main__':
     #tl.draw(pathsThruMidpoint.choice())
     print "number of paths through midpoint: " + str(pathsThruMidpoint.len())
     #print paths.universe()
-    #for p in pathsThruMidpoint:
-    #    print p
+    for p in pathsThruMidpoint:
+        print p
 
     #dim = (dimension[0]+1,dimension[1]+1)
     #""" AC: SAVE ZDD TO FILE

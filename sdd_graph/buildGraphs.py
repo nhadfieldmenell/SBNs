c23b6cc7badc0f766aa1c69a9be89c9a4880ac55
Noah <noah@ark.com>
2016-03-06 16:04:07 -0800

    find best point

--- sdd_graph/buildGraphs.py
+++ sdd_graph/buildGraphs.py
@@ -545,13 +545,13 @@ def main():
     
 
     #print g.trip_id2line_num
-    print g.trip_id2line_num[20]
+    print g.node2trip_ids[g.best_node][20:40]
     for i in range(100,125):
         trip_id =  g.node2trip_ids[g.best_node][i]
         line_num = g.trip_id2line_num[trip_id]
         p = Path(trip_id,g,line_num)
-        print p.trip_id
-        print p.path
+        #print p.trip_id
+        #print p.path
         p.print_path()
 
     return

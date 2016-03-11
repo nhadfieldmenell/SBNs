9b5d678f60dff87ba3b1f812592552eefb76bff2
Noah <noah@ark.com>
2016-03-11 10:01:29 -0800

    finding path bug

--- sdd_graph/test_grids_minato.py
+++ sdd_graph/test_grids_minato.py
@@ -56,7 +56,7 @@ def print_edge_numbering(universe,rows,cols):
             if edge_num < 10:
                 sys.stdout.write(" ")
         sys.stdout.write("O\n\n")
-        if row == len(rows-1):
+        if row == len(rows)-1:
             break
         for col in range(cols):
             node = row*cols+col+1

9b04cd88acef3a4f4c361aaf72d003646b69d276
Noah <noah@ark.com>
2016-03-12 13:55:33 -0800

    even epochs

--- sdd_graph/create_edges.py
+++ sdd_graph/create_edges.py
@@ -5,8 +5,8 @@ import pickle
 import test_grids_minato as grid
 
 def main():
-    if len(sys.argv) != 4:
-        print "usage: %s [GRID-M] [GRID-N] [MIDPOINT]" % sys.argv[0]
+    if len(sys.argv) != 3:
+        print "usage: %s [GRID-M] [GRID-N]" % sys.argv[0]
         exit(1)
     dim = (int(sys.argv[1]),int(sys.argv[2]))
     dimension = (dim[0]-1,dim[1]-1)

315381e4131e2b5645b94027c3d319eccbd807ea
Noah <noah@ark.com>
2016-03-15 11:12:33 -0700

    filtering partials

--- sdd_graph/psdd/complete_partials.py
+++ sdd_graph/psdd/complete_partials.py
@@ -30,7 +30,7 @@ def epochs_partial(rows,cols,num_epochs,copy):
 
     full_lines = full_file.readlines()
     partials_lines = partials_file.readlines()
-    full_ints = map(lambda x: map(int,x[:-1]),full_lines)
+    full_ints = map(lambda x: map(int,x[:-1].split(',')),full_lines)
     print full_ints[0]
 
 

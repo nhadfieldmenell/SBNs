e9ef5e686ea28adba1100bf3e222a369a10c596e
Noah <noah@ark.com>
2016-03-17 10:17:47 -0700

    filtering partials

--- sdd_graph/psdd/complete_partials.py
+++ sdd_graph/psdd/complete_partials.py
@@ -77,9 +77,6 @@ def epochs_partial(rows,cols,num_epochs,copy):
         intermediate.close()
         model_ds = DataSet.read(intermediate_name)
         """
-        print model
-        tuple_model = tuple(model)
-        print tuple_model
         partial_model = full_and_part[i][1]
         if str(model) in bad_models:
             total_bad += 1

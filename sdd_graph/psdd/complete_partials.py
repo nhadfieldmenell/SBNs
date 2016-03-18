d97fc7bbcf12121090053e591de18e183e5fe52f
Noah <noah@ark.com>
2016-03-17 23:54:02 -0700

    midpoint/no midpoint

--- sdd_graph/psdd/complete_partials.py
+++ sdd_graph/psdd/complete_partials.py
@@ -258,6 +258,7 @@ def most_likely_completions(full_datasets,partial_epochs,partial_completed,num_e
                 if model.count(1) == partial_edge_count + 1:
                     best_one += 1
                 else:
+                    print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
                     best_other += 1
                 print "Count: %d" % (0-count)
                 draw_grid(model,rows,cols,edge2index)

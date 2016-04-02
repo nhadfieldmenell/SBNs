bf79bcfeb4408e0aa0b3ac92fe3a4499f98f1297
Noah <noah@ark.com>
2016-04-01 18:26:48 -0700

    adapting to cab data

--- sdd_graph/psdd/complete_partials.py
+++ sdd_graph/psdd/complete_partials.py
@@ -141,13 +141,13 @@ def endpoint_dist(prediction,full,partial,midpoint,rows,cols,edge2index,edge_ind
     while cur_point != (-1,-1):
         print cur_point
         prev_point = cur_point
-        cur_point,prev_edge = find_next_point(prev_edge,prediction,mid_point,rows,cols,edge2index,edge_index2tuple)
+        cur_point,prev_edge = find_next_point(prev_edge,prediction,cur_point,rows,cols,edge2index,edge_index2tuple)
     last_point_prediction = prev_point
     prev_point = mid_point
     cur_point,prev_edge = find_next_point(orig_edge,full,mid_point,rows,cols,edge2index,edge_index2tuple)
     while cur_point != (-1,-1):
         prev_point = cur_point
-        cur_point,prev_edge = find_next_point(prev_edge,full,mid_point,rows,cols,edge2index,edge_index2tuple)
+        cur_point,prev_edge = find_next_point(prev_edge,full,cur_point,rows,cols,edge2index,edge_index2tuple)
     last_point_actual = prev_point
     return tuple_dist(last_point_prediction,last_point_actual) 
 

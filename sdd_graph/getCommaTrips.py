a1a2c3c56f1cdfe5a1bc7507033feb95a534f683
Noah <noah@ark.com>
2016-04-05 13:09:13 -0700

    flipping trips

--- sdd_graph/getCommaTrips.py
+++ sdd_graph/getCommaTrips.py
@@ -14,7 +14,8 @@ def writeTrip(tripNum,orig,out):
             break
 	
 #orig = open('csvGPS.txt','r')
-orig = open('cab_trips.txt','r')
+#orig = open('cab_trips.txt','r')
+orig = open('cab_chronological.txt','r')
 out = open('getTripsOut.txt','w')
 numArgs = len(sys.argv)
 for i in range(1,numArgs):

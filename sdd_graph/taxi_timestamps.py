ea28c93e722eed040aaf50609764c918816468d2
Noah <nhadfieldmenell@gmail.com>
2016-04-13 15:19:36 -0700

    uber directory

--- sdd_graph/taxi_timestamps.py
+++ sdd_graph/taxi_timestamps.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python
 import pickle
+from datetime.datetime import fromtimestamp
 import buildGraphs as bg
 
 def get_unix_time(line):
-    print "getting time"
     i = len(line)-1
     while line[i] != ',':
         i -= 1
     i += 1
     return int(line[i:])
+
+def convert_unix_time(unix):
+    """return [day_of_week (Monday is 0),hour,minute]"""
+    date = fromtimestamp(unix)
+    print "date object: %s" % str(date)
+    day = date.day + (date.month-5)*31
+    day_of_week = day % 7 - 5
+    if day_of_week < 0:
+        day_of_week += 7
+    print "day of week: %d" % day_of_week
+    return [day_of_week,date.hour,date.minute]
+
 
 def create_mappings(in_filename,out_filename):
     """Create pickled dict mapping from trip_id to trip start time
@@ -19,13 +31,16 @@ def create_mappings(in_filename,out_filename):
     with open(in_filename,'r') as infile:
         lines = infile.readlines()
 
-    print "loading"
-
     trip_id2line_num = pickle.load(open('trip_id2line_num.pickle','rb'))
 
-    print "loaded"
+    for i in (5,77,15):
+        line_num = trip_id2line_num[i]
+        line = lines[line_num]
+        unix = get_unix_time(line)
+        time_obj = convert_unix_time(unix)
+        print time_obj
 
-    print get_unix_time(lines[trip_id2line_num[66]])
+    #print get_unix_time(lines[trip_id2line_num[66]])
 
 def main():
     create_mappings('cab_chronological.txt','trip_id2time.pickle')

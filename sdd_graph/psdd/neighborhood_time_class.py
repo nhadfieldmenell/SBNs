3ebca7f5e83bbea82d1b5c8363793d62ca495144
Noah <nhadfieldmenell@gmail.com>
2016-04-20 15:54:03 -0700

    splitting trips on time

--- sdd_graph/psdd/neighborhood_time_class.py
+++ sdd_graph/psdd/neighborhood_time_class.py
@@ -117,7 +117,6 @@ def filter_bad(copy,in_fn,bad_fn,rows,cols,edge2index):
                 data.append(model)
 
         print "total bad: %d, unique bad: %d, total good: %d, unique good: %d" % (total_bad,unique_bad, total_good, unique_good)
-        full_dataset = []
 
         bad_file = open(bad_fn,'w')
         for i in bad_indices.keys():
@@ -125,7 +124,7 @@ def filter_bad(copy,in_fn,bad_fn,rows,cols,edge2index):
         bad_file.close()
 
         counts = [1 for j in range(len(full_ints))]
-        full_dataset.append(DataSet.to_dict(data,counts))
+        full_dataset = DataSet.to_dict(data,counts)
 
         return full_dataset
 
@@ -150,9 +149,8 @@ def filter_bad(copy,in_fn,bad_fn,rows,cols,edge2index):
                     data.append(model)
 
         print "total bad: %d, total good: %d" % (len(bad_paths), total_good)
-        full_dataset = []
         counts = [1 for j in range(len(full_ints))]
-        full_dataset.append(DataSet.to_dict(data,counts))
+        full_dataset = DataSet.to_dict(data,counts)
 
         print "num bad paths: %d" % len(bad_paths)
         return full_dataset

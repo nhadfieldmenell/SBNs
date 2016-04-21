bd55132c031061e0c91f077fa9ccb1c1c4ff13be
Noah <nhadfieldmenell@gmail.com>
2016-04-20 18:49:17 -0700

    unsupervised no metadata

--- sdd_graph/gen_general_ends.py
+++ sdd_graph/gen_general_ends.py
@@ -7,7 +7,6 @@ import sdd
 import test_grids_minato as minato
 
 def all_paths(dimension,dim):
-    print "here"
     from graphillion import GraphSet
     import graphillion.tutorial as tl
     start,goal = 1,(dimension[0]+1)*(dimension[1]+1)
@@ -58,3 +57,6 @@ def main():
     import graphillion.tutorial as tl
 
     all_paths(dimension,dim)
+
+if __name__ == '__main__':
+    main()

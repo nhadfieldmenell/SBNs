61ad30c066df6cfbd25957e5562c4ebfcd3da62f
Noah <nhadfieldmenell@gmail.com>
2016-04-25 19:19:50 -0700

    path manager

--- sdd_graph/psdd/fixed_ends_analyze.py
+++ sdd_graph/psdd/fixed_ends_analyze.py
@@ -56,22 +56,22 @@ class PathManager(object):
                     edge = (i*m+j+1,i*m+j+2)
                     index = self.edge2index[edge]
                     if model[index] == 1:
-                        sys.stdout.write(' ----- ')
+                        sys.stdout.write(' ---- ')
                     elif index in edge_num2prob.keys():
-                        sys.stdout.write(' %s ' % ('%.4f' % edge_num2prob[index])[1:])
+                        sys.stdout.write(' %s ' % ('%.3f' % edge_num2prob[index])[1:])
                     else:
-                        sys.stdout.write('       ')
+                        sys.stdout.write('      ')
             sys.stdout.write('\n')
             if i < m-1:
                 for j in xrange(n):
                     edge = (i*m+j+1,i*m+m+j+1)
                     index = self.edge2index[edge]
                     if model[index] == 1:
-                        sys.stdout.write('  |     ')
+                        sys.stdout.write('  |    ')
                     elif index in edge_num2prob.keys():
-                        sys.stdout.write('%s ' % ('%.4f' % edge_num2prob[index])[1:])
+                        sys.stdout.write('%s ' % ('%.3f' % edge_num2prob[index])[1:])
                     else:
-                        sys.stdout.write('        ')
+                        sys.stdout.write('      ')
             sys.stdout.write('\n')
 
     def most_likely_start(self,start,end):

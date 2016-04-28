05f9fd0fdd6573ed0fcae218377cc8c877c06782
Noah <nhadfieldmenell@gmail.com>
2016-04-28 10:31:24 -0700

    best all at once

--- sdd_graph/psdd/fixed_ends_analyze.py
+++ sdd_graph/psdd/fixed_ends_analyze.py
@@ -63,7 +63,10 @@ class PathManager(object):
                     if model[index] == 1:
                         sys.stdout.write(' ----- ')
                     elif index in edge_num2prob.keys():
-                        sys.stdout.write(' %s ' % ('%.4f' % edge_num2prob[index])[1:])
+                        if edge_num2prob[index] > 0.999:
+                            sys.stdout.write("   1   ")
+                        else:
+                            sys.stdout.write(' %s ' % ('%.4f' % edge_num2prob[index])[1:])
                     else:
                         sys.stdout.write('       ')
             sys.stdout.write('\n')
@@ -74,7 +77,10 @@ class PathManager(object):
                     if model[index] == 1:
                         sys.stdout.write('  |     ')
                     elif index in edge_num2prob.keys():
-                        sys.stdout.write('%s   ' % ('%.4f' % edge_num2prob[index])[1:])
+                        if edge_num2prob[index] > 0.999:
+                            sys.stdout.write("  1     ")
+                        else:
+                            sys.stdout.write('%s   ' % ('%.4f' % edge_num2prob[index])[1:])
                     else:
                         sys.stdout.write('        ')
             sys.stdout.write('\n')

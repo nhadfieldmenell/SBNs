259a915ec08f6b4b54cafffc9420e57cb76032de
Noah <nhadfieldmenell@gmail.com>
2016-04-28 12:06:00 -0700

    best all at once

--- sdd_graph/buildGraphs.py
+++ sdd_graph/buildGraphs.py
@@ -68,10 +68,7 @@ class Graph(object):
                 nodes[node_tup[1]] = True
         with open(out_fn,'w') as outfile:
             for node in nodes.keys():
-                print node
                 coords = self.node_to_coords(node)
-                print coords
-                print ""
                 outfile.write("%s\n" % str(self.coords_to_gps(coords))[1:-1])
 
 

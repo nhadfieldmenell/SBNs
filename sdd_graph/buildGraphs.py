68d4d8b02d9f4fea8b18940cbd82c7ef5821b05c
Noah <nhadfieldmenell@gmail.com>
2016-05-02 12:54:51 -0700

    step for 10x10

--- sdd_graph/buildGraphs.py
+++ sdd_graph/buildGraphs.py
@@ -982,14 +982,14 @@ def main():
     """
 
     g = Graph(full_fn,min_lat,max_lat,min_lon,max_lon,rows,cols)
-    #test_lat,test_lon = 37.793364, -122.409793 
-    #coords = g.gps_to_coords(test_lat,test_lon)
-    #print g.coords_to_node(coords[0],coords[1])
-    #test_lat,test_lon = 37.754396, -122.420007
-    #coords = g.gps_to_coords(test_lat,test_lon)
-    #print g.coords_to_node(coords[0],coords[1])
+    test_lat,test_lon = 37.793364, -122.409793 
+    coords = g.gps_to_coords(test_lat,test_lon)
+    print g.coords_to_node(coords[0],coords[1])
+    test_lat,test_lon = 37.754396, -122.420007
+    coords = g.gps_to_coords(test_lat,test_lon)
+    print g.coords_to_node(coords[0],coords[1])
 
-
+    return
     all_at_once_prefix = "psdd/paths/all_%d_%d_%d_%d" % (rows,cols,start,end)
     all_at_once_in = "%s.pickle" % all_at_once_prefix
     all_at_once_out = "%s_coords.txt" % all_at_once_prefix

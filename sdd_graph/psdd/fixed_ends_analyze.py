b671255ae2ee0dab5b941c3d986190c7c9d3be10
Noah <nhadfieldmenell@gmail.com>
2016-05-03 13:55:03 -0700

    nearest neighbor

--- sdd_graph/psdd/fixed_ends_analyze.py
+++ sdd_graph/psdd/fixed_ends_analyze.py
@@ -1006,7 +1006,7 @@ def test_nearest_neighbor(rows,cols,edge2index,edge_index2tuple):
 
     for i in (4,6,14,24,33,43):
         edge_path1[i] = 1
-    for j in (4,10,12,13,14,15,16,26,37,36,33,43):
+    for i in (4,10,12,13,14,15,16,26,37,36,33,43):
         edge_path2[i] = 1
 
     haus,sum_haus = man.min_and_sum_hausdorff(edge_path1,edge_path2)

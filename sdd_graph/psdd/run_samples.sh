98b96251cfac0bac33694753b7054facdbf81387
Noah <nhadfieldmenell@gmail.com>
2016-05-03 14:46:06 -0700

    run a bunch

--- sdd_graph/psdd/run_samples.sh
+++ sdd_graph/psdd/run_samples.sh
@@ -3,7 +3,7 @@
 starts=(18 15 90 62 32 17 77 38 39 23 47)
 ends=(73 88 22 6 89 93 19 75 81 86 71)
 
-for ((run=0;run<11,;run++))
+for ((run=0;run<11;run++))
 do
 	filename="output/step_vs_at_once_10_10_"${starts[$run]}"_"${ends[$run]}".txt"
 	pypy fixed_ends_analyze.py 10 10 ${starts[$run]} ${ends[$run]} > $filename

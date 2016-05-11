672584a88f1a8383e0091e730467c810206b1cb7
Noah <nhadfieldmenell@gmail.com>
2016-05-10 20:26:24 -0700

    trip_id2first_last

---
+++ sdd_graph/create_trip_id2first_last.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import pickle
+import sys
+
+def main():
+    row = int(sys.argv[1])
+    col = int(sys.argv[2])
+    first_last2trip_id = pickle.load(open('pickles/first_last2trip_ids-%d-%d.pickle' % (row,col),'rb'))
+    trip_id2first_last = {}
+    for first_last in first_last2trip_ids:
+        trips = first_last2trip_ids[first_last]
+        for trip in trips:
+            trip_id2first_last[trip] = first_last
+
+    with open('pickles/trip_id2first_last-%d-%d.pickle' % (row,col),'wb') as output:
+        pickle.dump(trip_id2first_last,output)
+
+if __name__ == '__main__':
+    main()

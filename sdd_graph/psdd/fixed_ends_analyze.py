d043b1af6a3b03b5a769d58b03c342ad92b99e41
Noah <nhadfieldmenell@gmail.com>
2016-05-12 11:18:26 -0700

    predicting only taken routes

--- sdd_graph/psdd/fixed_ends_analyze.py
+++ sdd_graph/psdd/fixed_ends_analyze.py
@@ -1333,11 +1333,11 @@ def main():
     bad_fn_general = 'bad_paths/general_bad-%d-%d.txt' % (rows,cols)
  
     #find_kl(rows,cols,fn_prefix_general,bad_fn_general,data_fn_general)
-    man = PathManager(rows,cols,edge2index,edge_index2tuple)
+    #man = PathManager(rows,cols,edge2index,edge_index2tuple)
     #man.create_first_last2models(data_fn_general,bad_fn_general)
     #man.analyze_paths_taken()
-    man.compare_observed_models()
-    return
+    #man.compare_observed_models()
+    #return
 
 
     copy = generate_copy(rows,cols,start,end,fn_prefix_general,data_fn_general,bad_fn_general,edge2index,num_edges)

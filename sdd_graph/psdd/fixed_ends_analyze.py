b215d8e7efe53a5d98b83716436e762c68d0fa85
Noah <nhadfieldmenell@gmail.com>
2016-05-13 11:25:24 -0700

    new bad paths

--- sdd_graph/psdd/fixed_ends_analyze.py
+++ sdd_graph/psdd/fixed_ends_analyze.py
@@ -1387,11 +1387,11 @@ def main():
  
     #find_kl(rows,cols,fn_prefix_general,bad_fn_general,data_fn_general)
     man = PathManager(rows,cols,edge2index,edge_index2tuple)
-    #man.analyze_predictions()
+    man.analyze_predictions()
     #man.create_first_last2models(data_fn_general,bad_fn_general)
     #man.analyze_paths_taken()
     #man.compare_observed_models()
-    #return
+    return
 
 
     copy = generate_copy(rows,cols,start,end,fn_prefix_general,data_fn_general,bad_fn_general,edge2index,num_edges)

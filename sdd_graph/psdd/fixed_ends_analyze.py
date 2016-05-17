9339dd05a4303f5cc463006acf1ea8cfcfbf120b
Noah <nhadfieldmenell@gmail.com>
2016-05-17 00:17:12 -0700

    Horowitz

--- sdd_graph/psdd/fixed_ends_analyze.py
+++ sdd_graph/psdd/fixed_ends_analyze.py
@@ -242,7 +242,7 @@ class PathManager(object):
                 for j in range(i,len(model_array)):
                     weight = weights_with_diag[i][j]
                     haus,sum_haus,DSN = self.path_diff_measures(model_array[i],model_array[j])
-                    #print "%s: haus %.2f, sum_haus %.2f, DSN %.2f" % (str((i,j)),haus,sum_haus,DSN) 
+                    print "%s: haus %.2f, sum_haus %.2f, DSN %.2f" % (str((i,j)),haus,sum_haus,DSN) 
                     fl2similarity_measures[fl][0] += weight*haus
                     fl2similarity_measures[fl][1] += weight*sum_haus
                     fl2similarity_measures[fl][2] += weight*DSN

a11ab32d8646ffd07dc4000e8bd85d0a8f7ebefc
Noah <nhadfieldmenell@gmail.com>
2016-05-18 15:37:32 -0700

    better trip translation

--- sdd_graph/psdd/split_trips.py
+++ sdd_graph/psdd/split_trips.py
@@ -6,11 +6,42 @@ import pickle
 
 def main():
     t2bad = pickle.load(open('../pickles/trip_id2bad_better.pickle','rb'))
-    t2model = pickle.load(open('../pickles/trip_id2model_better.pickle','rb'))
+    #t2model = pickle.load(open('../pickles/trip_id2model_better.pickle','rb'))
+    all_ids = []
     for i in range(1,464046):
-        if i not in t2model:
-            print i
-    print "all values have models"
+        if i not in t2bad:
+            all_ids.append(i)
+    print "%d good instances" % len(all_ids)
+    random.shuffle(all_ids)
+    tenth = len(all_ids)/10
+    t_id2training = {}
+    t_id2testing = {}
+    for i in range(2*tenth):
+        t_id2testing[all_ids[i]] = True
+    for i in range(2*tenth+1,len(all_ids)):
+        t_id2training[all_ids[i]] = True
+    print "Testing instances"
+    count = 0
+    for t in t_id2testing:
+        print t
+        if count > 10:
+            break
+        count += 1
+    print "Training instances"
+    count = 0
+    for t in t_id2training:
+        print t
+        if count > 10:
+            break
+        count += 1
+
+    for t in t_id2testing:
+        if t in t_id2training:
+            print "%d in both!"
+    for t in t_id2training:
+        if t in t_id2testing:
+            print "%d in both!"
+
     return
 
 

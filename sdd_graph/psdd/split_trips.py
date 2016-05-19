6fbbe26e1e0dabebcc1f59e9095e2419a8bd8eaf
Noah <nhadfieldmenell@gmail.com>
2016-05-18 21:20:12 -0700

    better trip translation

--- sdd_graph/psdd/split_trips.py
+++ sdd_graph/psdd/split_trips.py
@@ -4,7 +4,7 @@ import pickle
 
 
 def main():
-    t2bad = pickle.load(open('../pickles/trip_id2bad_better.pickle','rb'))
+    t2bad = pickle.load(open('better_pickles/trip_id2bad.pickle','rb'))
     #t2model = pickle.load(open('../pickles/trip_id2model_better.pickle','rb'))
     all_ids = []
     for i in range(1,464046):
@@ -22,9 +22,9 @@ def main():
     for i in range(2*tenth,len(all_ids)):
         t_id2training[all_ids[i]] = True
 
-    with open('pickles/t2testing.pickle','wb') as output:
+    with open('better_pickles/t2testing.pickle','wb') as output:
         pickle.dump(t_id2testing,output)
-    with open('pickles/t2training.pickle','wb') as output:
+    with open('better_pickles/t2training.pickle','wb') as output:
         pickle.dump(t_id2training,output)
 
     

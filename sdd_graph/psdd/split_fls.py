99cb5b0fa2d999fd5f9baf80767dcd8f012b4fbc
Noah <nhadfieldmenell@gmail.com>
2016-05-18 21:02:16 -0700

    better trip translation

--- sdd_graph/psdd/split_fls.py
+++ sdd_graph/psdd/split_fls.py
@@ -9,12 +9,28 @@ def main():
     ordered_fl2in = {}
     for fl in fl2ts:
         ordered_fl2in[(min(fl[0],fl[1]),max(fl[0],fl[1]))] = True
-    print len(fl2ts)
-    print len(ordered_fl2in)
     for fl in ordered_fl2in:
         fls.append(fl)
 
-    print len(fls)
+    num_splits = 8
+    block_size = len(fls)/num_splits
+    count = 0
+    for i in range(num_splits):
+        fn = 'better_pickles/fl_split%d.pickle' % i
+        fl2epoch = {}
+        if i == num_splits-1:
+            for j in range(i*block_size,len(fls)):
+                fl2epoch[fls[j]] = True
+        else:
+            for j in range(i*block_size,(i+1)*block_size):
+                fl2epoch[fls[j]] = True
+
+        count += len(fl2epoch)
+        with open(fn % (self.rows,self.cols),'wb') as output:
+            pickle.dump(fl2epoch,output)
+    print count
+
+
 
 
 

5ca637ac7c0da4b9ef726ebde09df2b5a145fdc0
Noah <nhadfieldmenell@gmail.com>
2016-05-25 23:07:58 -0700

    understanding similarity

--- sdd_graph/psdd/fixed_ends_analyze.py
+++ sdd_graph/psdd/fixed_ends_analyze.py
@@ -319,7 +319,7 @@ class PathManager(object):
             m_count = len(model2ts[model])
         haus,ampsd,dsn = self.evaluate_prediction_vs_models(best_model,model2ts)
         haus_p,ampsd_p,dsn_p = self.evaluate_prediction_vs_models(prediction,model2ts)
-        if haus_p <= haus or ampsd_p <= ampsd or dsn_p <= dsn:
+        if haus_p < haus or ampsd_p < ampsd or dsn_p < dsn:
             #print fl
             #self.understand_similarity(fl)
             return True
